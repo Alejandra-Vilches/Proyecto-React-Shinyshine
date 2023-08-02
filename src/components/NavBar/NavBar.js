c5ad577b650e5ebe7f0a288940ec98b9aba15eaf
Alejandra Constanza Vilches Villenas <avilchesv@MacBook-Pro-de-Ale.local>
2023-08-02 18:56:17 -0400

    primeros pasos proyecto react

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h2>Shinyshine</h2>
+            <div>
+                <button> Beauty Boxes</button>
+                <button> Beauty Packs</button>
+                <button> Uñas Impress</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
